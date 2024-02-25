a1d8ad70c05537ae8ddb6ed38ef7709f2bac09c8
Biresh <js.biresh@gmail.com>
2024-02-25 17:32:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const renderCountry = function (data, className = '') {
+  const htmlSnippet = `<article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${data.population}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', htmlSnippet);
+  countriesContainer.style.opacity = 1;
+};
+
+// const renderCountry = function (data) {
+//     let htmlSnippet = `<article class="country">
+//       <img class="country__img" src="${data.flag}" />
+//       <div class="country__data">
+//         <h3 class="country__name">${data.name}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${data.population}</p>`;
+  
+//     // Check if languages array is present and not empty
+//     if (data.languages && data.languages.length > 0) {
+//       htmlSnippet += `<p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>`;
+//     }
+  
+//     // Check if currencies array is present and not empty
+//     if (data.currencies && data.currencies.length > 0) {
+//       htmlSnippet += `<p class="country__row"><span>💰</span>${data.currencies[0].name}</p>`;
+//     }
+  
+//     htmlSnippet += `</div></article>`;
+  
+//     countriesContainer.insertAdjacentHTML('beforeend', htmlSnippet);
+//     countriesContainer.style.opacity = 1;
+//   };
+  
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    // Render Counrty
+    renderCountry(data);
+
+    // Get Neighbour Country
+    const neighbour = data.borders?.[0];
+    console.log(neighbour)
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function() {
+        const data2 = JSON.parse(this.responseText);
+        
+        renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('egypt');
+getCountryAndNeighbour('usa');
+
+
